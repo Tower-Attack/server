828d27f8f36c8071c2829662ba3ba9d85a5b40e4
Marlena Baker <baker.marlena@gmail.com>
2017-07-28 11:58:43 -0600

    one tower route

--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,8 @@ const knex = require('./knex')
 module.exports = {
   getAllTowers: ()=>{
     return knex('tower');
+  },
+  getOneTower: (id) => {
+    return knex('tower').where('id', id).first();
   }
 }
--- routes/fight.js
+++ routes/fight.js
@@ -9,4 +9,18 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.get('/:id', (req, res, next)=>{
+  queries.getOneTower(req.params.id)
+    .then(tower=>{
+      if(tower){
+        console.log(tower)
+        res.send(tower)
+      }
+      else{
+        res.status(400)
+        next(new Error('Does not exist.'))
+      }
+    })
+})
+
 module.exports = router;
